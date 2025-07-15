2bd96d1cdc2b81a0012c33b9bceae7f0e5bd4ffa
NiaNia2 <lacodeuse@protonmail.com>
2025-07-15 14:03:07 +0200

    second commit

--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -1,30 +1,30 @@
-// import { Controller } from '@hotwired/stimulus';
-// import ChapoSoundController from './chapo_sound_controller';
+import { Controller } from '@hotwired/stimulus';
 
-// /*
-//  * This is an example Stimulus controller!
-//  *
-//  * Any element with a data-controller="hello" attribute will cause
-//  * this controller to be executed. The name "hello" comes from the filename:
-//  * hello_controller.js -> "hello"
-//  *
-//  * Delete this file or adapt it for your use!
-//  */
-// export default class extends Controller {
-//     static values = {
-//         sound: String
-//     }
 
-//     connect() {
-//         console.log('✅ Stimulus hello connecté');
-//         this.audio = new Audio(this.soundValue);
-//     }
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    static values = {
+        sound: String
+    }
 
-//     play() {
-//         console.log('chapi.mp3');
+    connect() {
+        console.log('✅ Stimulus hello connecté');
+        this.audio = new Audio(this.soundValue);
+    }
 
-//         this.audio.currentTime = 0;
-//         this.audio.play().catch(e => console.warn("Audio bloqué :", e));
-//     }
-// }
+    play() {
+        console.log('chapi.mp3');
+
+        this.audio.currentTime = 0;
+        this.audio.play().catch(e => console.warn("Audio bloqué :", e));
+    }
+}
 
